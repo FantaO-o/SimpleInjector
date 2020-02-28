85b584de8149955fd2122f4dc680876e0913c8a9
FantaO-o <w60113@student.wsiz.rzeszow.pl>
2020-02-28 08:51:37 +0100

    WIP

---
+++ SimpleInjector.cpp
@@ -0,0 +1,14 @@
+// SimpleInjector.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <windows.h>
+#include <TlHelp32.h>
+#include <iostream>
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
